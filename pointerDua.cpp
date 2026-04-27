96206bb6929b397f93346ae4a252d7dd7664d623
canzonadzka <canzonadzka@gmail.com>
2026-04-27 12:05:20 +0700

    deklarasi awal

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -1,23 +1,3 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa
-{
-public:
-    int nim;
-    void showNim()
-    {
-        cout << "No Induk = " << nim << endl;
-    }
-};
-
-int main()
-{
-    // Pointer Object mhs menggunakan alokasi dinamis
-    mahasiswa *mhs = new mahasiswa; 
-    mhs->nim = 2;
-    mhs->showNim();
-    
-    delete mhs; // Menghapus memori yang dialokasikan
-    return 0;
-}
