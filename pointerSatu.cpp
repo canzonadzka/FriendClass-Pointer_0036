621532d03b44895850c830a635fa29dfe3e09732
canzonadzka <canzonadzka@gmail.com>
2026-04-27 12:03:21 +0700

    int main sekaligus penyelesaian

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -9,3 +9,17 @@ public:
     }
 };
 
+int main() {
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); // Member Access Operator
+
+    mahasiswa &refMhs = mhs; // Pointer Reference refMhs
+    refMhs.nim = 2; // Member Access Operator
+    refMhs.showNim();
+
+    mahasiswa *pMhs = &mhs; // Pointer Dereference pMhs
+    pMhs->nim = 3; // Arrow Operator
+    pMhs->showNim();
+
+    return 0;
+}
